26fdbdf4b871b01858ff61dea40c6b7ce958522f
Phil Yeeles <phil@yeel.es>
2012-05-01 20:10:52 +0100

    added emms, tidied up init.el a bit

--- init.el
+++ init.el
@@ -41,7 +41,7 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(TeX-engine (quote xetex))
- '(TeX-view-program-selection (quote (((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi") (output-pdf "xpdf") (output-html "xdg-open")))))
+ '(TeX-view-program-selection (quote (((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi") (output-pdf "Evince") (output-html "xdg-open")))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
@@ -53,7 +53,3 @@
  '(ediff-odd-diff-B ((((class color) (background dark)) (:background "dark red"))))
  '(mumamo-background-chunk-major ((((class color) (background dark)) (:background "black"))))
  '(mumamo-background-chunk-submode1 ((((class color) (background dark)) (:background "black")))))
-
-;; load go-mode
-(add-to-list 'load-path "./lib/go-mode-load.el" t)
-   (require 'go-mode-load)
